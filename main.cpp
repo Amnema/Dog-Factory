01ae6c32c322ddb1edf7ebc29b7366fa416090de
Dasha <lyapustina.dasha@gmail.com>
2024-10-04 21:45:32 +0500

    created classes.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "classes.cpp"
+
+using namespace std;
+int main()
+{
+    Alabai Misha (ColorEnum::White, SizeEnum::Giant);
+    Misha.Pet();
+    Misha.Feed();
+    Misha.Pet();
+    return 0;
+}
